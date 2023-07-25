fe23597f347eb22afc305ce1ffbc6d923cd80821
Panth Shah <89696311+Panth1823@users.noreply.github.com>
2023-07-25 22:38:50 +0530

    Add files via upload

--- form/3/a.js
+++ form/3/a.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 function clicked(){
     document.getElementById("clickbtn").textContent = "Clicked";
    
 
     }
+=======
+function clicked(){
+    document.getElementById("clickbtn").textContent = "Clicked";
+   
+
+    }
+>>>>>>> 050bdde805c800300b9cec1fe3ab351478cb14b5
     
