01ff3dac960db2079ad476cf2067415744cca20b
panth1823 <panthshah242@gmail.com>
2023-07-30 23:23:37 +0530

    new

---
+++ JS_/RandomNumber/main.js
@@ -0,0 +1,9 @@
+function ranNum(){
+
+let num = Math.floor(Math.random() * 205) + 1;
+
+// document.querySelector(".result").textContent =`Your random number is ${num}`;
+console.log(num);
+}
+
+ranNum();
